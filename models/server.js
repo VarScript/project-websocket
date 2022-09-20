e4ee1b7d9b209115060cec8c6eff90684cbe1813
DevHars <vargassanchezfabio01@gmail.com>
2022-09-20 08:40:46 -0500

    install socket.io

--- models/server.js
+++ models/server.js
@@ -3,8 +3,10 @@ const cors = require('cors');
 
 class Server {
     constructor() {
-        this.app = express(); 
-        this.port = process.env.PORT;
+        this.app    =  express(); 
+        this.port   =  process.env.PORT;
+        this.server =  require('http').createServer( this.app );
+        this.io     =  require('socket.io')(this.server);
 
         this.paths = {};
 
@@ -30,7 +32,7 @@ class Server {
     }
 
     listen() {
-        this.app.listen( this.port, () => {
+        this.server.listen( this.port, () => {
             console.log('Server runing in the port: ', this.port);
         });
     }
