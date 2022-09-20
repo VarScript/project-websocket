0a8dc1b88684d346822ab3c5587084c3a1606de2
DevHars <vargassanchezfabio01@gmail.com>
2022-09-20 09:32:36 -0500

    Mesage connect and diconnect

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,3 +1,23 @@
+// Referencs HTLM
 
+const lblOnline  = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
 
 const socket = io();
+
+
+
+socket.on('connect', () => {
+    console.log('Connected');
+
+    lblOffline.style.display = 'none';
+    lblOnline.style.display  = '';
+
+});
+
+socket.on('disconnect', () => {
+    console.log('Disconnected of the server');
+
+    lblOnline.style.display  = 'none';
+    lblOffline.style.display = '';
+})
