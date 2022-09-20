0132456ec50f637bb56db52db2dd91589ebf22bb
DevHars <vargassanchezfabio01@gmail.com>
2022-09-20 08:26:26 -0500

    Start project

---
+++ models/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+
+class Server {
+    constructor() {
+        this.app = express(); 
+        this.port = process.env.PORT;
+
+        this.paths = {};
+
+        // Middleware: functions that go add other functionality to my webserver
+        this.middleware();
+
+        // Routes of my application
+        this.routes();
+    }
+
+    middleware() {
+        // CORS
+        this.app.use(cors());
+
+        // Public directory
+        this.app.use( express.static('public')); // use: is the clave word
+    }
+
+    // Routes that i wanna
+    routes() {
+        
+        // this.app.use(this.paths.auth, require('../routes/auth.routes'));
+    }
+
+    listen() {
+        this.app.listen( this.port, () => {
+            console.log('Server runing in the port: ', this.port);
+        });
+    }
+}
+
+
+
+module.exports = Server;
